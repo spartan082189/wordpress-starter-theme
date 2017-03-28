1f28feb7b5cc72044e043caa625d768f39a7f1bb
Jamar Gillam <jamargillam@gmail.com>
2017-03-27 22:51:19 -0500

    More blog styles;

--- templates/blog/entry-summary.php
+++ templates/blog/entry-summary.php
@@ -1,5 +1,5 @@
 <section>
-<?php the_post_thumbnail('post-thumbnail', ['class' => 'uk-responsive-width', 'title' => 'Feature image']); ?>
-<?php the_excerpt(); ?>
+<?php //the_post_thumbnail('post-thumbnail', ['class' => 'uk-responsive-width', 'title' => 'Feature image']); ?>
+<p class="uk-text-lead"><?php the_excerpt(); ?></p>
 <?php if( is_search() ) { ?><div class="entry-links"><?php wp_link_pages(); ?></div><?php } ?>
 </section>
--- templates/blog/entry.php
+++ templates/blog/entry.php
@@ -1,22 +1,18 @@
-<article class="uk-padding">
-    <h2>
-      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="teal-text text-lighten-2"><?php the_title(); ?></a>
+<article class="uk-padding uk-article">
+    <h2 class="uk-article-title">
+      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="uk-link-reset"><?php the_title(); ?></a>
     </h2>
-    <?php
-    if (is_archive() || is_search()) {
-        get_template_part('/templates/blog/entry-summary');
-    } else {
-        // get_template_part('/templates/blog/entry-content');
-        get_template_part('/templates/blog/entry-summary');
-    }
+    <p class="uk-article-meta">Written by <a href="#"><?php the_author(); ?></a> on 12 April 2012. Posted in <a href="#">Blog</a></p>
+    <?php get_template_part('/templates/blog/entry-summary'); ?>
+    <?php get_template_part('templates/blog/entry-footer'); ?>
+    <a class="uk-button uk-button-text" href="<?php the_permalink(); ?>" title="<?php the_title();?>">Read more</a>
+    <p>
+    <?php 
+        $categories = get_the_category(); 
+        foreach ($categories as $category) {
+         echo '<a class="uk-label uk-display-inline-block" href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>';
+        }
     ?>
-    <?php
-    if (is_single()) {
-        get_template_part('entry-footer', 'single');
-    } else {
-        get_template_part('templates/blog/entry-footer');
-    }
-    ?>
-    <a class="waves-effect waves-light btn btn-large" href="<?php the_permalink(); ?>" title="<?php the_title();?>">Read full article...</a>
-    <p><?php the_category(); ?></p>
+    </p>
+    <hr class="uk-divider-icon">
 </article>
