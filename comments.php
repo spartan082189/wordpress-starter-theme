b9d033c43a497c060b6decfe988dc692eaa99720
Jamar Gillam <jamargillam@gmail.com>
2017-04-13 20:57:29 -0500

    More comment style/functionality changes

--- comments.php
+++ comments.php
@@ -1,50 +1,44 @@
-<?php $fields= array( 'author'=> '
-<p class="comment-form-author">
-  <label for="author">' . __( 'Name', 'domainreference' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) . '
-  <input id="author" class="uk-input" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
-    '" size="30" ' . $aria_req . ' />
-</p>', 'email' => '
-<p class="comment-form-email">
-  <label for="email">' . __( 'Email', 'domainreference' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) . '
-  <input id="email" class="uk-input" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
-    '" size="30" ' . $aria_req . ' />
-</p>' ); $args = array( 'form' => array( 'class' => 'form-horizontal' ), 'fields' => apply_filters('comment_form_default_fields', $fields), 'comment_field' => '
-<div class="form-group">' . '
-  <label for="comment">' . __( 'Comment', 'wp_babobski' ) . '</label><span>*</span>' . '
-  <textarea id="comment" class="uk-textarea" name="comment" rows="3" aria-required="true"></textarea>
-  <p id="d3" class="text-danger"></p>' . '</div>', 'comment_notes_after' => '', 'class_submit' => 'uk-button uk-button-primary' ); ?>
+<?php 
+    $fields = array( 
+        'author'=> '
+            <p class="comment-form-author">
+                <input class="uk-input" name="author" type="text" placeholder="Name" value="' . esc_attr( $commenter['comment_author'] ) .'" size="30" ' . $aria_req . ' />
+            </p>', 
+        'email' => '
+            <p class="comment-form-email">
+             <input class="uk-input" name="email" type="text" placeholder="Email" value="' . esc_attr(  $commenter['comment_author_email'] ) .'" size="30" ' . $aria_req . ' />
+            </p>' 
+        ); 
+
+    $args = array( 
+        'form' => array( 'class' => 'form-horizontal' ), 
+        'fields' => apply_filters('comment_form_default_fields', $fields), 
+        'comment_field' => '<textarea class="uk-textarea" placeholder="Comment" name="comment" rows="3" aria-required="true"></textarea>', 
+        'comment_notes_after' => '', 
+        'class_submit' => 'uk-button uk-button-primary' 
+        ); 
+?>
 
 <?php if ( 'comments.php'== basename( $_SERVER[ 'SCRIPT_FILENAME'] ) ) return; ?>
 <section class="uk-container uk-container-large">
   <div>
     <?php if ( have_comments() ) : global $comments_by_type; $comments_by_type=& separate_comments( $comments ); if ( ! empty( $comments_by_type[ 'comment'] ) ) : ?>
-    <section id="comments-list" class="comments">
-        <h3 class="comments-title"><?php comments_number(); ?></h3>
-        <?php if ( get_comment_pages_count()> 1 ) : ?>
-        <nav id="comments-nav-above" class="comments-navigation" role="navigation">
-        <div class="paginated-comments-links">
-            <?php paginate_comments_links(); ?>
-        </div>
-        </nav>
-        <?php endif; ?>
-        <ul>
+    <section id="comments-list">
+      <h3 class="comments-count">
+        <?php comments_number(); ?>
+      </h3>
+      <ul class="uk-list list-comments">
         <?php wp_list_comments( 'type=comment' ); ?>
-        </ul>
-        <?php if ( get_comment_pages_count()> 1 ) : ?>
-        <nav id="comments-nav-below" class="comments-navigation" role="navigation">
-        <div class="paginated-comments-links">
-            <?php paginate_comments_links(); ?>
+      </ul>
+      <?php if ( get_comment_pages_count()> 1 ) : ?>
+      <nav class="comments-navigation" role="navigation">
+        <div class="comments-links">
+          <?php paginate_comments_links(); ?>
         </div>
-        </nav>
-        <?php endif; ?>
+      </nav>
+      <?php endif; ?>
     </section>
-    <?php endif; if ( ! empty( $comments_by_type[ 'pings'] ) ) : $ping_count= count( $comments_by_type[ 'pings'] ); ?>
-    <section id="trackbacks-list" class="comments">
-        <h3 class="comments-title"><?php echo '<span class="ping-count">' . $ping_count . '</span> ' . ( $ping_count > 1 ? __( 'Trackbacks', 'blankslate' ) : __( 'Trackback', 'blankslate' ) ); ?></h3>
-        <ul>
-        <?php wp_list_comments( 'type=pings&callback=blankslate_custom_pings' ); ?>
-        </ul>
-    </section>
-    <?php endif; endif; if ( comments_open() ) comment_form($args); ?>
+    <?php endif; ?>
+    <?php endif; if ( comments_open() ) comment_form($args); ?>
   </div>
 </section>
--- templates/blog/single-loop.php
+++ templates/blog/single-loop.php
@@ -1,26 +1,31 @@
 <?php if (have_posts()) while (have_posts()) : the_post(); ?>
-  <div id="single-header" class="uk-padding-small">
-    <section class="uk-container">
-        <div class="uk-text-center">
-          <h1><?php the_title(); ?></h1>
-          <div class="post-date"><span uk-icon="icon: calendar"></span><span class="the-date"><?php echo the_date(); ?></span></div>
-          <div class="the-post-cats">
-            <?php 
+<div id="single-header" class="uk-padding-small">
+  <section class="uk-container">
+    <div class="uk-text-center">
+      <h1>
+        <?php the_title(); ?>
+      </h1>
+      <div class="post-date"><span uk-icon="icon: calendar"></span><span class="the-date"><?php echo the_date(); ?></span></div>
+      <div class="the-post-cats">
+        <?php 
                 $categories = get_the_category(); 
                 foreach ($categories as $category) {
                     echo '<a class="uk-label uk-display-inline-block" href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>';
                 }
             ?>
-            </div>
-        </div>
-    </section>
-  </div> <!--/#single-header-->
-  <div id="single-post">
-    <section class="uk-container uk-container-large">
-      <div uk-grid class="uk-grid-collapse">
-        <div class="uk-width-2-3@m">
+      </div>
+    </div>
+  </section>
+</div>
+<!--/#single-header-->
+<div id="single-post">
+  <section class="uk-container uk-container-large">
+    <div uk-grid class="uk-grid-collapse">
+      <div class="uk-width-2-3@m">
         <div class="uk-padding uk-padding-remove-top">
-          <div class="the-content"><?php the_content(); ?></div>
+          <div class="the-content">
+            <?php the_content(); ?>
+          </div>
           <div class="the-post-cats">
             <?php 
                 $categories = get_the_category(); 
@@ -28,7 +33,7 @@
                     echo '<a class="uk-label uk-display-inline-block" href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>';
                 }
             ?>
-            </div>
+          </div>
           <?php wp_link_pages(array('before' => '<div class="page-link">' . __('Pages:', 'twentyten'), 'after' => '</div>')); ?>
           <?php edit_post_link(__('Edit', 'twentyten'), '<span class="edit-link">', '</span>'); ?>
           <div class="uk-text-center post-pagination">
@@ -38,32 +43,31 @@
             <div title="Older post">
               <?php previous_post_link('%link', '<span class="meta-nav">' . _x('<span uk-icon="icon: chevron-left">', 'Previous post link', 'twentyten') . '</span> %title'); ?>
             </div>
-          </div><!-- #nav-below -->
           </div>
         </div>
-        <?php endwhile; // end of the loop. ?>
-      <div class="uk-width-1-3@m">
-          <?php get_sidebar(); ?>
+        <div id="comments">
+          <?php if ( ! post_password_required() ) comments_template( '', true );?>
+        </div>
       </div>
-    </div><!--/.row-->
+      <?php endwhile; // end of the loop. ?>
+      <div class="uk-width-1-3@m">
+        <?php get_sidebar(); ?>
+      </div>
+    </div>
   </section>
-</div>
-<div id="comments">
-    <?php if ( ! post_password_required() ) comments_template( '', true );?>
 </div>
 <footer id="about-footer">
   <section class="container">
-      <div class="row about">
-          <div class="col m5">
-              <img class="responsive-img z-depth-1" src="<?php echo get_stylesheet_directory_uri(); ?>/img/about-author.jpg" alt="My face :)"
-              title="My face :)" />
-          </div>
-          <div class="col m7 center-align">
-              <h3 class="teal-text text-lighten-2">About the author</h3>
-              <p>Michael Gillam is a Web Application Developer living in the western suburbs of Chicago in Naperville, IL. He is
-                  currently working as a Software Developer at <a href="https://www.adgooroo.com/" target="_blank">AdGooroo</a>
-                  in Chicago, IL. In his spare time, he enjoys gaming, coding, watching movies, traveling and road biking.</p>
-          </div>
-      </div><!--/.row-->
+    <div class="row about">
+      <div class="col m5">
+        <img class="responsive-img z-depth-1" src="<?php echo get_stylesheet_directory_uri(); ?>/img/about-author.jpg" alt="My face :)" title="My face :)" />
+      </div>
+      <div class="col m7 center-align">
+        <h3 class="teal-text text-lighten-2">About the author</h3>
+        <p>Michael Gillam is a Web Application Developer living in the western suburbs of Chicago in Naperville, IL. He is currently working as a Software Developer at <a href="https://www.adgooroo.com/" target="_blank">AdGooroo</a> in Chicago, IL. In his
+          spare time, he enjoys gaming, coding, watching movies, traveling and road biking.</p>
+      </div>
+    </div>
+    <!--/.row-->
   </section>
 </footer>
