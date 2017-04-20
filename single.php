8154bfe847072f1b00507d0535b30b62f3529fe6
Jamar Gillam <jamargillam@gmail.com>
2017-04-19 22:40:24 -0500

    Finished up single template (mainly)

--- single.php
+++ single.php
@@ -2,4 +2,5 @@
   <section id="single">
     <?php get_template_part('/templates/blog/single-loop'); ?>
   </section>
+<?php get_template_part('templates/globals/offcanvas-menu'); ?>
 <?php get_footer();?>
--- templates/blog/single-loop.php
+++ templates/blog/single-loop.php
@@ -1,5 +1,6 @@
 <?php if (have_posts()) while (have_posts()) : the_post(); ?>
-<div id="single-header" class="uk-padding-small">
+<div id="single-header" class="uk-padding-small" style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
+<?php get_template_part('templates/globals/navigation'); ?>
   <section class="uk-container">
     <div class="uk-text-center">
       <h1>
@@ -8,11 +9,16 @@
       <div class="post-date"><span uk-icon="icon: calendar"></span><span class="the-date"><?php echo the_date(); ?></span></div>
       <div class="the-post-cats">
         <?php 
-                $categories = get_the_category(); 
-                foreach ($categories as $category) {
-                    echo '<a class="uk-label uk-display-inline-block" href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>';
-                }
-            ?>
+          $categories = get_the_category(); 
+          foreach ($categories as $category) {
+            echo '<a class="uk-label uk-display-inline-block" href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>';
+          }
+        ?>
+      </div>
+      <div class="share-icons uk-padding-small">
+        <a href="" class="uk-margin-small-right" uk-icon="icon: twitter; ratio: 1.5"></a>
+        <a href="http://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>" class="uk-margin-small-right" uk-icon="icon: facebook; ratio: 1.5"></a>
+        <a href="" class="" uk-icon="icon: google-plus; ratio: 1.5"></a>
       </div>
     </div>
   </section>
@@ -56,18 +62,10 @@
     </div>
   </section>
 </div>
-<footer id="about-footer">
-  <section class="container">
-    <div class="row about">
-      <div class="col m5">
-        <img class="responsive-img z-depth-1" src="<?php echo get_stylesheet_directory_uri(); ?>/img/about-author.jpg" alt="My face :)" title="My face :)" />
-      </div>
-      <div class="col m7 center-align">
-        <h3 class="teal-text text-lighten-2">About the author</h3>
-        <p>Michael Gillam is a Web Application Developer living in the western suburbs of Chicago in Naperville, IL. He is currently working as a Software Developer at <a href="https://www.adgooroo.com/" target="_blank">AdGooroo</a> in Chicago, IL. In his
-          spare time, he enjoys gaming, coding, watching movies, traveling and road biking.</p>
-      </div>
+<footer id="about-footer" class="uk-padding">
+  <section class="uk-container uk-container-large">
+    <div uk-grid class="uk-grid-collapse">
+      <div class="uk-width-3-3@m">Something about you goes here...</div>
     </div>
-    <!--/.row-->
   </section>
 </footer>
