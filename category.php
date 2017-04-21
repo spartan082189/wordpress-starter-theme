3c67c6d74037259472ebdc2e03530613b5b69079
Jamar Gillam <jamargillam@gmail.com>
2017-04-20 21:16:45 -0500

    Updated category template

--- category.php
+++ category.php
@@ -1,13 +1,29 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<header class="header">
-<h1 class="entry-title"><?php _e( 'Category Archives: ', 'blankslate' ); ?><?php single_cat_title(); ?></h1>
-<?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<div class="archive-meta">' . category_description() . '</div>' ); ?>
-</header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
-</section>
-<?php get_sidebar(); ?>
+<div id="category">
+    <section id="cat-hero">
+        <?php get_template_part('templates/globals/navigation'); ?>
+        <div class="uk-container uk-container-large">
+            <h1 uk-scrollspy="cls:uk-animation-slide-top-small; delay: 100; repeat: true"><?php single_cat_title(); ?></h1>
+        </div>
+    </section>    
+    <section id="cat-posts">
+        <div class="uk-container uk-container-large">
+            <div uk-grid class="uk-grid-collapse">
+                <div class="uk-width-2-3@m">
+                    <?php
+                    $categorydesc = category_description();
+                    if (!empty($categorydesc))
+                        echo apply_filters('archive_meta', '<div class="archive-meta">' . $categorydesc . '</div>');
+                    ?>
+                    <?php rewind_posts(); ?>
+                    <?php while (have_posts()) : the_post(); ?>
+                        <?php get_template_part('templates/blog/entry-simple'); ?>
+                    <?php endwhile; ?>
+                </div>
+                <div class="uk-width-1-3@m"><?php get_sidebar(); ?></div>
+            </div>
+        </div>    
+    </section>    
+</div>
+<?php get_template_part('templates/globals/offcanvas-menu'); ?>    
 <?php get_footer(); ?>
